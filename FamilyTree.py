d0baf2f1659e1017fd2aa9bb89f2479926fceb0c
VivianBelle17 <dcvivian36@gmail.com>
2026-02-20 00:02:13 -0800

    Added menu functions

--- FamilyTree.py
+++ FamilyTree.py
@@ -13,9 +13,34 @@ class FamilyTree:
 
         self.people.extend([person_one, person_two])
 
+    def total_people(self):
+        return len(self.people)
+    
+    def total_decade(self):
+        count = {}
+
+        for p in self.people:
+            decade = (p.year_born // 10) * 10
+            count[decade] = count.get(decade, 0) + 1
+
+        return count
+    
+
+    def duplicate_names(self):
+        name_count = {}
+
+        for p in self.people:
+            name = f"{p.first_name} {p.last_name}"
+            name_count[name] = name_count.get(name, 0) + 1
+
+        return [name for name, count in name_count.items() if count > 1]
+        
+
+
 
 if __name__ == "__main__":
     tree = FamilyTree()
 
     tree.generate_initial_people()
     print(len(tree.people))
+    print(tree.duplicate_names())

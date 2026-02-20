65f1993e9381a0bab9dbeb544f3d44507080cca5
VivianBelle17 <dcvivian36@gmail.com>
2026-02-19 22:37:01 -0800

    Added a generate year died function

--- PersonFactory.py
+++ PersonFactory.py
@@ -3,6 +3,7 @@
 # get_person(year_born), read_files()
 
 import pandas as pd
+import random as rand
 
 class PersonFactory:
     def __init__(self):
@@ -15,7 +16,21 @@ class PersonFactory:
 
     def read_files(self):
         self.life_expectancy = pd.read_csv('life_expectancy.csv')
-        self.first_name = pd.read_csv("first_names.csv")
+        self.first_names = pd.read_csv("first_names.csv")
         self.last_names = pd.read_csv("last_names.csv")
         self.rank_to_prob = pd.read_csv("rank_to_probability.csv")
         self.birth_marriage_rate = pd.read_csv("birth_and_marriage_rates.csv")
+
+    def generate_year_died(self, year_born):
+        length = rand.randint(-10, 10)
+
+        index = self.life_expectancy["Year"] == year_born
+
+        # Returns the whole row so you need to get the value at the str column
+        life_expec = self.life_expectancy[index]["Period life expectancy at birth"].values[0]
+        
+        year_died = int(life_expec) + length + year_born
+
+        return year_died
+
+
